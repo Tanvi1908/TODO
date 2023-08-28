6546567206aec5eb8349d47fb712dcdecf5fb350
Tanvi <tmk.kr2016@gmail.com>
2023-08-28 15:14:08 -0400

    test1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { TableComponent } from './table/table.component';
 
 import { TodoComponent } from './todo/todo.component';
@@ -7,6 +8,7 @@ import { TodoComponent } from './todo/todo.component';
 const routes: Routes = [
 
   { path: '', component: TableComponent },
+  { path: 'home', component: HomeComponent }
 
 
 
